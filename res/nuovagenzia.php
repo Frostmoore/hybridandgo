3dc913a01c2cdcbbce21cf0c5aca4031e71c2a21
Frostmoore <r.ronconi@seemypage.it>
2024-01-12 01:40:34 +0100

    In teoria ho finito di impostare la pagina per creare una nuova agenzia

--- res/nuovagenzia.php
+++ res/nuovagenzia.php
@@ -129,6 +129,144 @@ if (!isset($_FILES["header_agenzia"])) {
     }
 }
 
+// Upload Immagine Info
+if (!isset($_FILES["info_immagine"])) {
+    die("Non è stato caricato alcun file nel capo Cover. La Cover è un elemento obbligatorio.");
+} else {
+
+    // Define Variables
+    $info_immaginePath = $_FILES["info_immagine"]["tmp_name"];
+    $info_immagineDimensions = @getimagesize($info_immaginePath);
+    $info_immagineWidth = $info_immagineDimensions[0];
+    $info_immagineHeight = $info_immagineDimensions[1];
+    $info_immagineSize = filesize($info_immaginePath);
+    $info_immagineInfo = finfo_open(FILEINFO_MIME_TYPE);
+    $info_immagineType = finfo_file($info_immagineInfo, $info_immaginePath);
+
+    // Check File Size
+    if ($info_immagineSize === 0) {
+        die("Il file Immagine Info è vuoto.");
+    } elseif ($info_immagineSize > 16145728) {
+        die("La Immagine Info che hai caricato è troppo pesante. Max 14MB.");
+    }
+
+    // Check File Type
+    if (!in_array($info_immagineType, $allowedTypes)) {
+        die("Questo tipo di file non è consentito per la Immagine Info.");
+    }
+
+    // Check File Dimensions
+    if ($info_immagineWidth > 2400) {
+        die("La tua Immagine Info è troppo larga.");
+    }
+
+    if ($info_immagineHeight > 1256) {
+        die("La tua Immagine Info è troppo alta.");
+    }
+
+    // Upload File
+    if ($_FILES["info_immagine"]["error"] > 0) {
+        echo "Error: " . $_FILES["info_immagine"]["error"] . "<br />";
+    } else {
+        move_uploaded_file($_FILES["info_immagine"]["tmp_name"], $dir . "info_immagine.png");
+
+        /* PATH TO Immagine Infoe */
+        $info_immagine = $dir . "info_immagine.png";
+    }
+}
+
+// Upload Immagine Contatti
+if (!isset($_FILES["contatti_immagine"])) {
+    die("Non è stato caricato alcun file nel capo Cover. La Cover è un elemento obbligatorio.");
+} else {
+
+    // Define Variables
+    $contatti_immaginePath = $_FILES["contatti_immagine"]["tmp_name"];
+    $contatti_immagineDimensions = @getimagesize($contatti_immaginePath);
+    $contatti_immagineWidth = $contatti_immagineDimensions[0];
+    $contatti_immagineHeight = $contatti_immagineDimensions[1];
+    $contatti_immagineSize = filesize($contatti_immaginePath);
+    $contatti_immagineInfo = finfo_open(FILEINFO_MIME_TYPE);
+    $contatti_immagineType = finfo_file($contatti_immagineInfo, $contatti_immaginePath);
+
+    // Check File Size
+    if ($contatti_immagineSize === 0) {
+        die("Il file Immagine Contatti è vuoto.");
+    } elseif ($contatti_immagineSize > 16145728) {
+        die("La Immagine Contatti che hai caricato è troppo pesante. Max 14MB.");
+    }
+
+    // Check File Type
+    if (!in_array($contatti_immagineType, $allowedTypes)) {
+        die("Questo tipo di file non è consentito per la Immagine Contatti.");
+    }
+
+    // Check File Dimensions
+    if ($contatti_immagineWidth > 2400) {
+        die("La tua Immagine Contatti è troppo larga.");
+    }
+
+    if ($contatti_immagineHeight > 1256) {
+        die("La tua Immagine Contatti è troppo alta.");
+    }
+
+    // Upload File
+    if ($_FILES["contatti_immagine"]["error"] > 0) {
+        echo "Error: " . $_FILES["contatti_immagine"]["error"] . "<br />";
+    } else {
+        move_uploaded_file($_FILES["contatti_immagine"]["tmp_name"], $dir . "contatti_immagine.png");
+
+        /* PATH TO Immagine Contattie */
+        $contatti_immagine = $dir . "contatti_immagine.png";
+    }
+}
+
+// Upload Immagine Denuncia Sinistro
+if (!isset($_FILES["denuncia_immagine"])) {
+    die("Non è stato caricato alcun file nel capo Cover. La Cover è un elemento obbligatorio.");
+} else {
+
+    // Define Variables
+    $denuncia_immaginePath = $_FILES["denuncia_immagine"]["tmp_name"];
+    $denuncia_immagineDimensions = @getimagesize($denuncia_immaginePath);
+    $denuncia_immagineWidth = $denuncia_immagineDimensions[0];
+    $denuncia_immagineHeight = $denuncia_immagineDimensions[1];
+    $denuncia_immagineSize = filesize($denuncia_immaginePath);
+    $denuncia_immagineInfo = finfo_open(FILEINFO_MIME_TYPE);
+    $denuncia_immagineType = finfo_file($denuncia_immagineInfo, $denuncia_immaginePath);
+
+    // Check File Size
+    if ($denuncia_immagineSize === 0) {
+        die("Il file Immagine Denuncia Sinistro è vuoto.");
+    } elseif ($denuncia_immagineSize > 16145728) {
+        die("La Immagine Denuncia Sinistro che hai caricato è troppo pesante. Max 14MB.");
+    }
+
+    // Check File Type
+    if (!in_array($denuncia_immagineType, $allowedTypes)) {
+        die("Questo tipo di file non è consentito per la Immagine Denuncia Sinistro.");
+    }
+
+    // Check File Dimensions
+    if ($denuncia_immagineWidth > 2400) {
+        die("La tua Immagine Denuncia Sinistro è troppo larga.");
+    }
+
+    if ($denuncia_immagineHeight > 1256) {
+        die("La tua Immagine Denuncia Sinistro è troppo alta.");
+    }
+
+    // Upload File
+    if ($_FILES["denuncia_immagine"]["error"] > 0) {
+        echo "Error: " . $_FILES["denuncia_immagine"]["error"] . "<br />";
+    } else {
+        move_uploaded_file($_FILES["denuncia_immagine"]["tmp_name"], $dir . "denuncia_immagine.png");
+
+        /* PATH TO Immagine Denuncia Sinistroe */
+        $denuncia_immagine = $dir . "denuncia_immagine.png";
+    }
+}
+
 // Generate API Key
 function generateRandomString($length = 10)
 {
@@ -155,8 +293,8 @@ $api_key = generateRandomString();*/
 // Sezione 1
 $nome_app = $_POST["nome_app"];
 $nome_agenzia = $_POST["nome_agenzia"];
-$logo_agenzia = $_POST["logo_agenzia"];
-$header_agenzia = $_POST["header_agenzia"];
+//$logo_agenzia = $_POST["logo_agenzia"];
+//$header_agenzia = $_POST["header_agenzia"];
 // Sezione 2
 $facebook_agenzia = $_POST["facebook_agenzia"];
 $instagram_agenzia = $_POST["instagram_agenzia"];
@@ -165,7 +303,7 @@ $google_agenzia = $_POST["google_agenzia"];
 $sito_agenzia = $_POST["sito_agenzia"];
 // Sezione 3
 $info_titolo = $_POST["info_titolo"];
-$info_immagine = $_POST["info_immagine"];
+//$info_immagine = $_POST["info_immagine"];
 $info_nomi_sedi = $_POST["info_nomi_sedi"];
 $info_indirizzi_sedi = $_POST["info_indirizzi_sedi"];
 $info_testo_orari = $_POST["info_testo_orari"];
@@ -180,14 +318,14 @@ $notifica_titolo = $_POST["notifica_titolo"];
 $notifica_testo = $_POST["notifica_testo"];
 $notifica_link = $_POST["notifica_link"];
 // Sezione 5
-$contatti_immagine = $_POST["contatti_immagine"];
+//$contatti_immagine = $_POST["contatti_immagine"];
 $contatti_titolo = $_POST["contatti_titolo"];
 $contatti_testo = $_POST["contatti_testo"];
 $numeri_utili_labels = $_POST["numeri_utili_labels"];
 $numeri_utili_colori = $_POST["numeri_utili_colori"];
 $numeri_utili_link = $_POST["numeri_utili_link"];
 // Sezione 6
-$denuncia_immagine = $_POST["denuncia_immagine"];
+//$denuncia_immagine = $_POST["denuncia_immagine"];
 $denuncia_titolo = $_POST["denuncia_titolo"];
 $denuncia_testo = $_POST["denuncia_testo"];
 $denuncia_testo_grassetto = $_POST["denuncia_testo_grassetto"];
