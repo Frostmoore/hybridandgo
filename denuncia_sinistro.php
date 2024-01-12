a18aa7472ae1c97e067227fef5a28f5dda9974ed
Frostmoore <r.ronconi@seemypage.it>
2024-01-12 21:48:17 +0100

    Aggiunto script a piede di denuncia sinistro, da fare ajax

--- denuncia_sinistro.php
+++ denuncia_sinistro.php
@@ -31,6 +31,9 @@ if (!isset($_GET['token'])) {
         die('Accesso non consentito');
     }
 }
+
+$token = $_GET['token'];
+$agenzia_id = $_GET['id'];
 ?>
 <html>
 
@@ -42,22 +45,170 @@ if (!isset($_GET['token'])) {
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
 </head>
 
 <body>
     <img src="res/img/<?= $agenzia['id']; ?>/header_agenzia.png" id="header_agenzia" class="header_agenzia">
     <div class="page_denuncia">
-        <h1 style="text-align:center;">Denuncia il tuo Sinistro su <?= $agenzia['nome_agenzia']; ?></h1>
-        <div class="row_selettore">
-            <select id="select_sinistro">
-                <option value="selezione" id="option_selezione">Scegli il tipo di Sinistro</option>
-                <option value="auto" id="option_auto">Sinistro Auto</option>
-                <option value="nonauto" id="option_nonauto">Altro Sinistro</option>
-            </select>
+        <h1 style="text-align:center;" class="h1_form_denuncia">Denuncia il tuo Sinistro su <?= $agenzia['nome_agenzia']; ?></h1>
+        <div class="row_selettore" id="row_selettore">
+            <button type="button" class="tasto_selezione_sinistro" name="button_auto" id="button_auto" onclick="dropdown_form('auto')">Sinistro Auto</button>
+            <button type="button" class="tasto_selezione_sinistro" name="button_nonauto" id="button_nonauto" onclick="dropdown_form('nonauto')">Altro Sinistro</button>
         </div>
-        <div class="form_auto_wrapper a_hidden" id="form_auto_wrapper"></div>
-        <div class="form_nonauto_wrapper a_hidden" id="form_nonauto_wrapper"></div>
+        <div class=" form_auto_wrapper a_hidden" id="form_auto_wrapper">
+            <form action="" method="post" enctype="multipart/form-data">
+                <div class="row_form_denuncia">
+                    <div class="form-group-denuncia">
+                        <label for="primo_nome_denuncia_auto" class="label_denuncia">Il tuo Nome<span style="color: red;">*</span></label><br />
+                        <input type="text" class="form-control" id="primo_nome_denuncia_auto" name="primo_nome_denuncia_auto" placeholder="Es. Mario">
+                    </div>
+                    <div class="form-group-denuncia">
+                        <label for="cognome_denuncia_auto" class="label_denuncia">Il tuo Cognome<span style="color: red;">*</span></label><br />
+                        <input type="text" class="form-control" id="cognome_denuncia_auto" name="cognome_denuncia_auto" placeholder="Es. Rossi">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="email_denuncia_auto" class="label_denuncia">Il tuo Indirizzo e-mail<span style="color: red;">*</span></label>
+                    <input type="text" class="form-control" id="email_denuncia_auto" name="email_denuncia_auto" placeholder="Es. mario.rossi@email.it">
+                    <small id="email_denuncia_autohelp" class="form-text text-muted">Questo sarà l'indirizzo al quale riceverai la risposta.</small>
+                </div>
+
+                <div class="form-group">
+                    <label for="descrizione_denuncia_auto" class="label_denuncia">Descrivi brevemente il tuo sinistro<span style="color: red;">*</span></label>
+                    <textarea class="form-control" id="descrizione_denuncia_auto" name="descrizione_denuncia_auto" rows="5"></textarea>
+                </div>
+
+                <div class="mb-3">
+                    <label for="cai_denuncia_auto" class="label_denuncia">Carica il tuo CAI compilato<span style="color: red;">*</span></label>
+                    <input class="form-control" type="file" id="cai_denuncia_auto" name="cai_denuncia_auto" multiple="multiple">
+                    <small id="cai_denuncia_autohelp" class="form-text text-muted">Puoi scattargli una foto o caricarlo in PDF!</small>
+                </div>
+
+                <div class="mb-3">
+                    <label for="documenti_denuncia_auto" class="label_denuncia">Carica fronte e retro della tua patente<span style=" color: red;">*</span></label>
+                    <input class="form-control" type="file" id="documenti_denuncia_auto" name="documenti_denuncia_auto" multiple="multiple">
+                    <small id="documenti_denuncia_autohelp" class="form-text text-muted">Puoi scattare due foto o caricarla in PDF!</small>
+                </div>
+
+                <div class="mb-3">
+                    <label for="immagini_denuncia_auto" class="label_denuncia">Carica immagini relative al tuo sinistro</label>
+                    <input class="form-control" type="file" id="immagini_denuncia_auto" name="immagini_denuncia_auto" multiple="multiple">
+                    <small id="immagini_denuncia_autohelp" class="form-text text-muted">Puoi scattare delle foto o caricarle direttamente.</small>
+                </div>
+
+
+                <div class="form-group">
+                    <h4>Dichiarazione di accettazione della liberatoria privacy</h4>
+                    <p class="form_privacy" id="form_privacy">Dichiaro di aver preso visione della <a href="https://luneziainsurance.it/privacy-e-cookie-policy/" id="privacy_link_auto">Privacy policy</a> e autorizzo l'agenzia <?= $agenzia['nome_agenzia']; ?> al trattamento dei miei dati personali, che saranno trattati ex Artt. 13-14 del Regolamento (UE) n. 679/2016 (c.d. G.D.P.R.) sulla protezione dei dati personali, per le finalità ivi indicate.
+                    </p>
+                    <div class="row_form_denuncia_center">
+                        <input type="checkbox" id="checkbox_privacy_auto" name="checkbox_privacy_auto" class="cb_denuncia">
+                        <label for="checkbox_privacy_auto" class="label_privacy">Do il consenso<span style="color: red;">*</span></label><br>
+                    </div>
+                </div>
+
+                <input type="text" class="a_hidden" id="agenzia_id" value="<?= $agenzia_id; ?>">
+                <div class="rowbottone">
+                    <button type="button" class="bottone_submit_denuncia_auto" id="bottone_submit_denuncia_auto" onclick="denunciaSinistro('<?= $agenzia_id; ?>', 'nonauto')">INOLTRA LA DENUNCIA</button>
+                </div>
+        </div>
+        </form>
+    </div>
+    <div class="page_denuncia">
+        <div class="form_nonauto_wrapper a_hidden" id="form_nonauto_wrapper">
+            <form action="" method="post" enctype="multipart/form-data">
+                <div class="row_form_denuncia">
+                    <div class="form-group-denuncia">
+                        <label for="primo_nome_denuncia_nonauto" class="label_denuncia">Il tuo Nome<span style="color: red;">*</span></label><br />
+                        <input type="text" class="form-control" id="primo_nome_denuncia_nonauto" name="primo_nome_denuncia_nonauto" placeholder="Es. Mario">
+                    </div>
+                    <div class="form-group-denuncia">
+                        <label for="cognome_denuncia_nonauto" class="label_denuncia">Il tuo Cognome<span style="color: red;">*</span></label><br />
+                        <input type="text" class="form-control" id="cognome_denuncia_nonauto" name="cognome_denuncia_nonauto" placeholder="Es. Rossi">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="email_denuncia_nonauto" class="label_denuncia">Il tuo Indirizzo e-mail<span style="color: red;">*</span></label>
+                    <input type="text" class="form-control" id="email_denuncia_nonauto" name="email_denuncia_nonauto" placeholder="Es. mario.rossi@email.it">
+                    <small id="email_denuncia_nonautohelp" class="form-text text-muted">Questo sarà l'indirizzo al quale riceverai la risposta.</small>
+                </div>
+                <div class="form-group">
+                    <label for="descrizione_denuncia_nonauto" class="label_denuncia">Descrivi brevemente il tuo sinistro<span style="color: red;">*</span></label>
+                    <textarea class="form-control" id="descrizione_denuncia_nonauto" name="descrizione_denuncia_nonauto" rows="5"></textarea>
+                </div>
+                <div class="mb-3">
+                    <label for="documenti_denuncia_nonauto" class="label_denuncia">Carica fronte e retro del documento di identità<span style=" color: red;">*</span></label>
+                    <input class="form-control" type="file" id="documenti_denuncia_nonauto" name="documenti_denuncia_nonauto" multiple="multiple">
+                    <small id="documenti_denuncia_nonautohelp" class="form-text text-muted">Puoi scattare due foto o caricarlo in PDF!</small>
+                </div>
+                <div class="mb-3">
+                    <label for="immagini_denuncia_nonauto" class="label_denuncia">Carica immagini relative al tuo sinistro</label>
+                    <input class="form-control" type="file" id="immagini_denuncia_nonauto" name="immagini_denuncia_nonauto" multiple="multiple">
+                    <small id="immagini_denuncia_nonautohelp" class="form-text text-muted">Puoi scattare delle foto o caricarle direttamente.</small>
+                </div>
+                <div class="form-group">
+                    <h4>Dichiarazione di accettazione della liberatoria privacy</h4>
+                    <p class="form_privacy" id="form_privacy">Dichiaro di aver preso visione della <a href="https://luneziainsurance.it/privacy-e-cookie-policy/" id="privacy_link_nonauto">Privacy policy</a> e autorizzo l'agenzia <?= $agenzia['nome_agenzia']; ?> al trattamento dei miei dati personali, che saranno trattati ex Artt. 13-14 del Regolamento (UE) n. 679/2016 (c.d. G.D.P.R.) sulla protezione dei dati personali, per le finalità ivi indicate.
+                    </p>
+                    <div class="row_form_denuncia_center">
+                        <input type="checkbox" id="checkbox_privacy_nonauto" name="checkbox_privacy_nonauto" class="cb_denuncia">
+                        <label for="checkbox_privacy_nonauto" class="label_privacy">Do il consenso<span style="color: red;">*</span></label><br>
+                    </div>
+                    <input type="text" class="a_hidden" id="agenzia_id" value="<?= $agenzia_id; ?>">
+                    <div class="rowbottone">
+                        <button type="button" class="bottone_submit_denuncia_nonauto" id="bottone_submit_denuncia_nonauto" onclick="denunciaSinistro('<?= $agenzia_id; ?>', 'nonauto')">INOLTRA LA DENUNCIA</button>
+                    </div>
+            </form>
+        </div>
     </div>
 </body>
+
+<script>
+    // campi generici
+    var form_nonauto_wrapper = $("#form_nonauto_wrapper");
+    var form_auto_wrapper = $("#form_auto_wrapper");
+    var select_sinistro = $("#select_sinistro");
+    var option_selezione = $("#option_selezione");
+    var option_auto = $("#option_auto");
+    var option_nonauto = $("#option_nonauto");
+    var row_selettore = $("#row_selettore");
+
+
+    // campi auto
+    var primo_nome_denuncia_auto = $("#primo_nome_denuncia_auto");
+    var cognome_denuncia_auto = $("#cognome_denuncia_auto");
+    var email_denuncia_auto = $("#email_denuncia_auto");
+    var descrizione_denuncia_auto = $("#descrizione_denuncia_auto");
+    var cai_denuncia_auto = $("#cai_denuncia_auto");
+    var documenti_denuncia_auto = $("#documenti_denuncia_auto");
+    var immagini_denuncia_auto = $("#immagini_denuncia_auto");
+
+    // campi non auto
+    var primo_nome_denuncia_nonauto = $("#primo_nome_denuncia_nonauto");
+    var cognome_denuncia_nonauto = $("#cognome_denuncia_nonauto");
+    var email_denuncia_nonauto = $("#email_denuncia_nonauto");
+    var descrizione_denuncia_nonauto = $("#descrizione_denuncia_nonauto");
+    var documenti_denuncia_nonauto = $("#documenti_denuncia_nonauto");
+    var immagini_denuncia_nonauto = $("#immagini_denuncia_nonauto");
+
+
+    // Funzionalità
+    function dropdown_form(value) {
+        switch (value) {
+            case 'auto':
+                form_auto_wrapper.fadeIn();
+                form_nonauto_wrapper.fadeOut();
+                row_selettore.fadeOut();
+                break;
+            case 'nonauto':
+                form_auto_wrapper.fadeOut();
+                form_nonauto_wrapper.fadeIn();
+                row_selettore.fadeOut();
+                break;
+        }
+    }
+</script>
 
 </html>
